6dab3537b367625a33feea9cb4bab040515e6731
Adarsh Kumar <99128631+MindedCode@users.noreply.github.com>
2022-07-19 22:27:56 +0530

    Create find size of dataty.cpp

---
+++ find size of dataty.cpp
@@ -0,0 +1,23 @@
+
+//find the program of size to datatype 
+//using class in C++
+#include<iostream>
+using namespace std;
+
+class Hero{
+    // characteristics
+     int life;
+};
+
+int main()
+{
+    // creation of  object
+   Hero l1;
+    
+   cout<<"size:"<<sizeof(l1)<<endl; 
+    return 0;
+}
+
+Output of the program :
+
+size : 4
