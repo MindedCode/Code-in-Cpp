c392e77762e93a901f643b05890d4934a2f3e722
Adarsh Kumar <99128631+MindedCode@users.noreply.github.com>
2022-08-03 23:59:16 +0530

    Create poiter to structure.cpp

---
+++ poiter to structure.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+struct student 
+{
+	int roll =12;
+	char name[10];
+};
+int main()
+
+{
+	struct student s={12,'mindedCode'
+	};
+	struct student *p = &s;
+	r.
+	cout<<r<<endl;
+	cout<<a<<endl;
+}
