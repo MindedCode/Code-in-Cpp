53ba0411084ea29a722b8a1b1804e692537d700d
Adarsh Kumar <99128631+MindedCode@users.noreply.github.com>
2022-08-04 19:58:53 +0530

    Create monolithic pro.cpp

---
+++ advance/monolithic pro.cpp
@@ -0,0 +1,24 @@
+**/this is a monolithic program/**
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+int main()
+{
+	int length = 0;
+	int breadth = 0;
+	
+	cout<<"Enter the area of length and breath"<<endl;
+	cin>>length>>breadth;
+//	cout<<length*breadth;
+	int area = breadth*length;
+	cout<<area;
+	
+	
+}
+
+output::
+Enter the area of length and breath
+12
+12
+144
